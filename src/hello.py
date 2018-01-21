1059286cb793a825b1c6a57cf540aecbb1cbf3d1
Mottaqui Karim <taqqui.karim@gmail.com>
2018-01-21 17:48:47 -0500

    added logging and twilioML lib to prcoess responses

--- src/hello.py
+++ src/hello.py
@@ -1,7 +1,16 @@
-from json import dumps
+import logging
+from twilio.twiml.messaging_response import MessagingResponse
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    resp = MessagingResponse()
+    resp.message("Hello, sir. I have awoken")
+
+    logger.info('Testing')
+
     return {
       'statusCode': 200,
-      'body': dumps({ 'hello': 'world!!!' })
+      'body': str(resp) 
     }
